a27bfe57371a6f75d2c30eebdcffeaa3596e490c
rithik-06 <rithiktiwari33@gmail.com>
2025-11-04 05:25:09 +0530

    setup vector database

---
+++ groq.py
@@ -0,0 +1,15 @@
+import openai
+
+
+
+client = openai.OpenAI(
+    api_key="your-groq-api-key",
+    base_url="https://api.groq.com/openai/v1"
+)
+
+response = client.responses.create(
+    model="llama-3.3-70b-versatile",
+    input="Tell me a fun fact about the moon in one sentence.",
+)
+
+print(response.output_text)
